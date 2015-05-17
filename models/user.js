2397264b0d9c8ca92c5ce5e5c6c383d26a641131
Rene Garcia <rene@generalui.com>
2015-05-17 11:59:18 -0700

    adding beer endpoint

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var BEER_CALORIES = 200;
 
 var userSchema = mongoose.Schema({
 	facebook_uid: String,
@@ -36,7 +36,50 @@ userSchema.methods.nutritionRating = function() {
 };
 
 userSchema.methods.numberOfBeers = function(caloriesBurned) {
-  
+  var decimalBeers = caloriesBurned/BEER_CALORIES;
+  var roundedBeers = Math.round( decimalBeers * 10 ) / 10;
+  return roundedBeers;
 };
+
+userSchema.methods.caloriesBurnedByRunning = function (distance,time) {  
+  //distance in miles
+  //time in hours
+  var calories = 0;
+  var met = 0;
+  var metArray = [6.0,8.3,9,9.8,10.5,11,11.4,11.8,12.3,12.8,14.5,16,19,19.8,23]
+  var knownSpeeds = [4,5,5.2,6,6.7,7,7.5,8,8.6,9,10,11,12,13,14];
+  var speed = (distance/time);
+  var roundedSpeed = closestSpeed(speed);
+  var weight = Number(userSchema.weight);
+  met = getMet(roundedSpeed);
+  calories = cckg(weight,met,time);
+  
+  /***Helpers functions***/
+  function cckg(w,met,t){ 
+    var kg = w*0.45359237
+    return Math.round(met * 3.5 * (kg) / 200 * time);
+  }
+
+  function closestSpeed(num) {  
+    var curr = knownSpeeds[0];
+    var diff = Math.abs (num - curr);
+    for (var val = 0; val < knownSpeeds.length; val++) {      
+      var newdiff = Math.abs (num - knownSpeeds[val]);
+      if (newdiff < diff) {   
+        diff = newdiff;
+        curr = knownSpeeds[val];
+      } 
+    }   
+    return curr;
+  }
+
+  function getMet(s){ 
+    var met=-1;
+    var metIndex = knownSpeeds.indexOf(s)
+    return metArray[metIndex]
+  }
+
+  return calories;
+}
 
 module.exports = mongoose.model('User', userSchema);
