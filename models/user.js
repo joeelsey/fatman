85458f4e6da44f6ab6ba30590e4336b54bb4e133
Joe Elsey <joe.elsey@gmail.com>
2015-05-16 20:56:36 -0700

    updated user mode and added schema method for finding nutrition rating

--- models/user.js
+++ models/user.js
@@ -13,8 +13,26 @@ var userSchema = mongoose.Schema({
   },
   date_of_birth: String,
   age: String,
-  dataSeted: Boolean
+  dataSeted: Boolean,
+  activity: String
 
 });
+
+userSchema.methods.nutritionRating = function() {
+  var USER_CM = (userSchema.height / 0.032808) + (userSchema.inches * 2.54);
+  var USER_WEIGHT = userSchema.weight * 0.45359237;
+  var nutritionRating;
+  if (userSchema.sex === "Male") {
+    var Men_REE = (9.99 * USER_WEIGHT) + (6.25 * USER_CM) - (4.92 * userSchema.age) + 5;
+    nutritionRating= Men_REE;
+  }
+
+  if (userSchema.sex === "Female") {
+    var Women_REE = (9.99 * USER_WEIGHT) + (6.25 * USER_CM) - (4.92 * userSchema.age) - 161;
+    nutritionRating = Women_REE;
+  }
+
+  return nutritionRating;
+};
 
 module.exports = mongoose.model('User', userSchema);
