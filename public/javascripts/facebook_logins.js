0f4661f66cbeb53f02756cad7af7f4e457bc5a11
Rene Garcia <rene@generalui.com>
2015-05-16 20:03:34 -0700

    adding main page

--- public/javascripts/facebook_logins.js
+++ public/javascripts/facebook_logins.js
@@ -21,8 +21,11 @@ $(document).ready(function() {
         $.post("/users/signin", user,function(data){
           console.log("return data: ", data);
           window.currentUser = new User(data);
-          console.log("User loged in!!!!");
-          console.log(window.currentUser);
+          var name = currentUser.name.split(" ")[0]
+          var numberBeers = currentUser.beers || 0
+          $("#main-page #user-name").text(name + " EARNED: ");
+          $("#main-page #user-beer").text(numberBeers);
+          $.mobile.changePage( "#main-page", { transition: "slide", changeHash: false });
         })
       });
     }
