2914de39961783c53bca13b2e75c05058158bdd1
Rene Garcia <rene@generalui.com>
2015-05-31 18:20:40 -0700

    adding cordova build file

--- mobileapp_builder.js
+++ mobileapp_builder.js
@@ -1,17 +1,24 @@
 var jade = require('jade');
+var async  = require('async');
 var fs = require('fs')
 var options = {
 	filename: "fatman"
 };
 var locals = {};
 var outputDirr = __dirname + '/' + 'cordova_files';
+var assetsRootFolder = __dirname + '/' + 'public/';
+var assetsFolders = ["images", "javascripts", "stylesheets"];
+
+function createOutputFolder(callback){
+	fs.mkdir(outputDirr, callback)
+}
 
 function writeFile(path, data, callback){
 	fs.writeFile(path, data, function (err) {
     if (err) {
       callback(err);
     } else {
-      console.log('Wrote: ' + path);
+      console.log('Wrote: ', path);
       callback();
     }
   });
@@ -23,21 +30,99 @@ function readAndCompileTemplate(path, callback){
 	callback(html);
 }
 
-function processTemplates(){
+function processTemplates(callback){
 	var indexPath = "./views/index.jade";
 	readAndCompileTemplate(indexPath, function(html){
 		var outputFilename = outputDirr + '/index.html';
-		writeFile(outputFilename, html,function(err){
-			if(err){
-				console.log("There was an error: ", err);
-				process.exit(1);
+		fs.exists(outputDirr, function(exists){
+			if(!exists){
+				createOutputFolder(function(err){
+					if(err){
+						callback(err);
+					}
+					else{
+						writeFile(outputFilename, html,callback)
+					}
+				})
 			}
 			else{
-				console.log("Finishing Process with success!!!");
-				process.exit(0);
+				writeFile(outputFilename, html,callback)
 			}
 		})
-	})
+	});
 }
-processTemplates();
+
+function processAssets(callback){
+	async.each(assetsFolders, readAssetsFolder, callback);
+}
+
+function readAssetsFolder(folderName, callback){
+	var fromFolderPath = assetsRootFolder + folderName;
+	var toFolderPath = outputDirr + '/' + folderName;
+	console.log("About to copy assets files from folder: ",folderName);
+	fs.exists(toFolderPath,function(exists){
+		if(exists){
+			readAndMoveFromFolder(folderName,fromFolderPath, callback);
+		}
+		else{
+			console.log("About to create asset folder: ", toFolderPath);
+			fs.mkdir(toFolderPath, function(err){
+				if(err){
+					console.log("failed to create folder: ", toFolderPath);
+					callback(err);
+				}
+				else{
+					readAndMoveFromFolder(folderName,fromFolderPath, callback);
+				}
+			})
+		}
+	});
+}
+
+function readAndMoveFromFolder(folderName,folderPath, callback){
+	fs.readdir(folderPath, function(err, files){
+		if(err){
+			callback(err);
+		}
+		else{
+			copyAssetsFolderToCordova(folderName,folderPath, files,callback)
+		}
+	});
+}
+
+function copyAssetsFolderToCordova(folderName,folderPath,files, callback){
+	async.each(files, function(fileName, callback){
+		if(fileName != ".DS_Store"){
+			var filePath = folderPath + "/" + fileName;
+			console.log("About to move file: ", filePath);
+			fs.readFile(filePath, function (err, data) {
+			  if (err){
+			  	callback(err)
+			  }
+			  else{
+			  	var outputFilename = outputDirr + '/' + folderName + '/' + fileName;
+			  	writeFile(outputFilename, data,callback)
+			  }
+			});
+		}
+	},callback)
+}
+
+function startProcessing(){
+	console.log("Starting building process of html/css/js files");
+	async.parallel([processTemplates, processAssets], doneProcess);
+}
+
+function doneProcess(err, results){
+	if(err){
+		console.log("There was an error: ", err);
+		process.exit(1);
+	}
+	else{
+		console.log("Finishing Process with success!!!");
+		process.exit(0);
+	}
+}
+
+startProcessing();
 
