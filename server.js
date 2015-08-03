508b33d4f29d1cf9f789a918ce80cfab80c24d6d
Joe Elsey <joe.elsey@gmail.com>
2015-08-02 20:49:30 -0700

    removed error handler on server js

--- server.js
+++ server.js
@@ -73,11 +73,11 @@ app.get('/sign_s3', function(req, res, next){
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 // error handlers
 
